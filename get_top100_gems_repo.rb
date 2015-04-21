16c45e539a0880a3723ce555ba27fcde0a950264
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-21 00:23:06 -0400

    ignore txt files

--- get_top100_gems_repo.rb
+++ get_top100_gems_repo.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require './run_command'
 
 gems = {}
-run("gem list").each_line do |line|
+run("gem list --remote").each_line do |line|
   gemname = line.split(/\s/).first.strip
   gem_info = JSON.parse(run "curl https://rubygems.org/api/v1/gems/#{gemname}.json")
   gems.merge!({ gem_info["name"] => { downloads: gem_info["downloads"].to_i,
@@ -12,7 +12,7 @@ run("gem list").each_line do |line|
                                       src: gem_info["source_code_uri"]}})
 end
 
-gems = gems.sort_by{|k, v| v[:downloads] }.reverse.first(3).to_h
+gems = gems.sort_by{|k, v| v[:downloads] }.reverse.first(100).to_h
 file = File.open("gems.yml", "w")
 file.write( gems.to_yaml)
 file.close
