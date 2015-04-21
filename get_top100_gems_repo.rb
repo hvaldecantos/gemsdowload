4c0f104e8dc9f589b921555c9538cf01efaab9a2
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-20 23:58:27 -0400

    write gem info to yaml file

---
+++ get_top100_gems_repo.rb
@@ -0,0 +1,18 @@
+require 'csv'
+require 'json'
+require 'yaml'
+require './run_command'
+
+gems = {}
+run("gem list").each_line do |line|
+  gemname = line.split(/\s/).first.strip
+  gem_info = JSON.parse(run "curl https://rubygems.org/api/v1/gems/#{gemname}.json")
+  gems.merge!({ gem_info["name"] => { downloads: gem_info["downloads"].to_i,
+                                      version: gem_info["version"],
+                                      src: gem_info["source_code_uri"]}})
+end
+
+gems = gems.sort_by{|k, v| v[:downloads] }.reverse.first(3).to_h
+file = File.open("gems.yml", "w")
+file.write( gems.to_yaml)
+file.close
