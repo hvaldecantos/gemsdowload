f9eb73d47453eaf43765b9ddd0848773430eac95
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-21 00:25:38 -0400

    add script to dowload all gemnames

---
+++ get_gems_names.rb
@@ -0,0 +1,13 @@
+require 'csv'
+require 'json'
+require 'yaml'
+require './run_command'
+
+num = 0
+file = File.open("gems.txt", "w")
+run("gem list --remote").each_line do |line|
+  file.write line
+  num += 1
+end
+file.close
+puts "#{num} gem names and versions were writen."
