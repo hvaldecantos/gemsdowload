eb4215b978f5d2ceefb30bd277c08eb794a9cfc2
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-21 00:31:22 -0400

    read from file to gather info data

--- get_top100_gems_repo.rb
+++ get_top100_gems_repo.rb
@@ -3,10 +3,13 @@ require 'json'
 require 'yaml'
 require './run_command'
 
+num = 0
 gems = {}
-run("gem list --remote").each_line do |line|
+
+File.open("gems.txt").each_line do |line|
   gemname = line.split(/\s/).first.strip
   gem_info = JSON.parse(run "curl https://rubygems.org/api/v1/gems/#{gemname}.json")
+  num +=1
   gems.merge!({ gem_info["name"] => { downloads: gem_info["downloads"].to_i,
                                       version: gem_info["version"],
                                       src: gem_info["source_code_uri"]}})
@@ -16,3 +19,4 @@ gems = gems.sort_by{|k, v| v[:downloads] }.reverse.first(100).to_h
 file = File.open("gems.yml", "w")
 file.write( gems.to_yaml)
 file.close
+puts "#{num} gem info was written."
