cedf5dff1e1bec92973c5b8a576b810cca65fdc8
Héctor Valdecantos <hvaldecantos@gmail.com>
2015-04-25 17:57:07 -0400

    add log when clonning, and add limit of 100

--- clone_top100_gems.rb
+++ clone_top100_gems.rb
@@ -6,24 +6,37 @@ require 'logger'
 begin
   cwd = Dir.getwd
   gems = YAML.load(File.open("gems.yml"))
-  logger = Logger.new('logfile.log')
+  logger = Logger.new('logfile_clone.log')
 
+  Dir.mkdir(cwd + '/../clonedgems/')
   Dir.chdir(cwd + '/../clonedgems/')
 
+  gnum = 0
+  gcloned = 0
+
   gems.each do |gemname, info|
+    gnum += 1
     if info[:src]
       begin
-        # puts "git clone #{info[:src]}" if info[:src].match(/github.com/)
-        run "git clone #{info[:src]}" if info[:src].match(/github.com/)
-        logger.info("Gem #{gemname} successful cloned.")
+        if info[:src].match(/github.com/).nil? == false
+          run "git clone #{info[:src]}"
+          gcloned += 1
+          logger.info("Gem #{gemname} successful cloned. [num: #{gnum}, cloned: #{gcloned}]")
+        else
+          logger.warn("Gem #{gemname} uri is notq from github. [num: #{gnum}]")
+        end
       rescue => e
-        logger.error("Cannot clone #{gemname}: #{e}")
+        logger.error("Cannot clone #{gemname}: #{e.message}")
       end
+    else
+      logger.warn("Gem #{gemname} no URI repo found. [num: #{gnum}]")
     end
+    break if gcloned == 100
   end
 
 rescue ArgumentError => e
   logger.error("Could not parse YAML: #{e}")
 ensure
   Dir.chdir cwd
+  logger.info("Clonning done.")
 end
